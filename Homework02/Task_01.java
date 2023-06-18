e157a071327f853b019dfd00345c6a496f04f60a
Vovqa <vovan102vb@gmail.com>
2023-06-18 14:56:19 +0300

    Initial commit

---
+++ Homework02/Task_01.java
@@ -0,0 +1,11 @@
+// Ctrl + .         рефакторинг
+// Shift + Alt + F  выравнивание кода
+// Shift + Alt + .  исправление кода
+
+package Homework02;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        //
+    }
+}
