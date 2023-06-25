c3eeb8320b45d5173fbff3d3362db7e5ca6cd1a5
Vovqa <vovan102vb@gmail.com>
2023-06-25 13:59:37 +0300

    Added task #1 for HW03

---
+++ Homework03/DoubleLinkedList.java
@@ -0,0 +1,93 @@
+package Homework03;
+
+// null <-> Node1 <-> Node2 <-> Node3 <-> null
+// двусвязный список
+public class DoubleLinkedList {
+    Node head;
+    Node tail;
+
+    public class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value = " + value +
+                    ", next = " + next +
+                    ", prev = " + prev +
+                    '}';
+        }
+    }
+    
+    /**
+     * Вывод списка в консоль
+     */
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.value + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * Добавление значения в конец списка
+     * @param value
+     */
+    public void addNext(int value) {
+        Node new_node = new Node(); // создали новую пустую ноду
+        new_node.value = value; // положили значение в новую ноду
+
+        // привяжем нашу новую ноду к концу списка, если таков существует
+        if (head == null) { // если головы нету, то список не существует - обозначаем ему начало и конец
+            head = new_node;
+            tail = new_node;
+        } else { // если голова найдена, то список есть - паркуем нашу ноду в конец
+            tail.next = new_node; // даём знать хвосту - что у него появился сосед справа
+            new_node.prev = tail; // даём знать новой ноде - что у неё появился сосед слева, подружили...
+            tail = new_node; // пусть хвостом будет новая нода, чтобы дальше можно было так же дружить остальных
+        }
+    }
+
+    /**
+     * Добавление значения в начало списка
+     * @param value
+     */
+    public void addFirst(int value) {
+        Node new_node = new Node(); // создали новую пустую ноду
+        new_node.value = value; // положили значение в новую ноду
+
+        // привяжем нашу новую ноду к началу списка, если таков существует
+        if (head == null) { // если головы нету, то список не существует - обозначаем ему начало
+            head = new_node;
+            tail = new_node;
+        } else { // если голова найдена, то список есть - паркуем нашу ноду в начало
+            head.prev = new_node; // даём знать голове - что у неё появился сосед слева
+            new_node.next = head; // даём знать новой ноде - что у неё появился сосед справа, подружили...
+            head = new_node; // пусть головой будет новая нода, чтобы дальше можно было так же дружить остальных
+        }
+    }
+
+    /**
+     * Разворот двусвязного списка через цикл
+     */
+    public void revert() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            Node previous = currentNode.prev;
+            currentNode.next = previous;
+            currentNode.prev = next;
+            if (previous == null) {
+                tail = currentNode;
+            }
+            if (next == null) {
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+}
---
+++ Homework03/Task_01.java
@@ -0,0 +1,18 @@
+package Homework03;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        DoubleLinkedList dll = new DoubleLinkedList();
+        dll.addNext(3);
+        dll.addNext(4);
+        dll.addNext(5);
+
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.addFirst(0);
+
+        dll.printList();
+        dll.revert();
+        dll.printList();
+    }
+}
